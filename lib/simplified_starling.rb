a59351cc861360592482a056f848284bdfa4e8a9
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-11 16:39:27 +0200

    Rewritten ...

--- lib/simplified_starling.rb
+++ lib/simplified_starling.rb
@@ -1,48 +1,35 @@
-#!/usr/bin/env ruby
+module Simplified
 
-##
-# Boot Rails
-#
-require File.dirname(__FILE__) + '/../../../../config/environment'
+  class Starling
 
-require 'rubygems'
-require 'starling'
-
-unless ARGV.size == 1
-  puts "== STARLING ERROR ========================================"
-  puts "=> Please, provide a queue name."
-  exit
-end
-
-##
-# Connect to Starling to check if the queue exists or not ...
-#
-
-def start_processing(queue)
-  pid = fork do
-    Signal.trap('HUP', 'IGNORE') # Don't die upon logout
-    loop do
-      job = STARLING.get(queue)
-      begin
-        job[:type].constantize.find(job[:id]).send(job[:task])
-      rescue
-        puts "Error on #{job}"
+    def self.prepare(queue)
+      if STARLING.available_queues.include?(queue)
+        start_processing(queue)
+      else
+        puts "== STARLING ERROR: Queue not available ==================="
       end
-      # sleep 5
+    rescue Exception => error
+      puts "== STARLING ERROR: #{error} ==============="
     end
-  end
-  Process.detach(pid)
-end
 
-begin
-  STARLING = Starling.new('127.0.0.1:22122')
-  puts STARLING.available_queues.inspect
-  if STARLING.available_queues.include?(ARGV.first)
-    puts "== STARTING STARLING PROCESSOR ==========================="
-    start_processing(ARGV.first)
-  else
-    puts "== STARLING ERROR: Queue not available ==================="
+    def self.start_processing(queue)
+      pid = fork do
+        Signal.trap('HUP', 'IGNORE')
+        loop do
+          job = STARLING.get(queue)
+          begin
+            job[:type].constantize.find(job[:id]).send(job[:task])
+          rescue
+            puts "Error on #{job}"
+          end
+        end
+      end
+      Process.detach(pid)
+      ##
+      # TODO: Store pid id in a file ...
+      puts "== STARTING STARLING PROCESSOR == #{pid} ===================="
+    end
+
   end
-rescue Exception => error
-  puts "== STARLING ERROR: #{error} ==============="
+
 end
