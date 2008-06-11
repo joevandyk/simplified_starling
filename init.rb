5177cfa81ac02646622b48dd6d329ab07526feb6
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-11 16:39:01 +0200

    Boot starling from the plugin

--- init.rb
+++ init.rb
@@ -1,1 +1,10 @@
-# Include hook code here
+require 'starling'
+require 'simplified_starling'
+
+##
+# TODO: Read config from a yaml file
+
+##
+# Raise an error if starling is not available
+
+STARLING = Starling.new('127.0.0.1:22122')
