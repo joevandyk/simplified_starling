10e390b1bee1f74386b0b49f0443032f66f77ffd
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-12 08:22:59 +0200

    Copy starling.yml on install

--- install.rb
+++ install.rb
@@ -1,2 +1,4 @@
-##
-# TODO: Copy yaml file to RAILS_ROOT/config/starling.yml
+require 'fileutils'
+
+starling_config = File.dirname(__FILE__) + '/../../../config/starling.yml'
+FileUtils.cp File.dirname(__FILE__) + '/config/starling.yml.tpl', starling_config unless File.exist?(starling_config)
