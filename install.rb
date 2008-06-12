6f00b71d0c5547782431f305f57d3af54be00eda
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-12 13:46:21 +0200

    Copy Starling initializer ...

--- install.rb
+++ install.rb
@@ -6,5 +6,11 @@ FileUtils.mkdir starling_folder unless File.exist?(starling_folder)
 
 %w( development test production ).each do |env|
   starling_config = File.dirname(__FILE__) + "/../../../config/starling/#{env}.yml"
-  FileUtils.cp File.dirname(__FILE__) + '/config/starling.yml.tpl', starling_config unless File.exist?(starling_config)
+  FileUtils.cp File.dirname(__FILE__) + '/files/config.yml.tpl', starling_config unless File.exist?(starling_config)
+end
+
+starling_initializer = File.dirname(__FILE__) + '/../../../config/initializers/starling.rb'
+
+unless File.exist?(starling_initializer)
+  FileUtils.cp File.dirname(__FILE__) + '/files/initializer.rb', starling_initializer
 end
