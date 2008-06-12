2a10a4b5de6d29dcfd5aa70afc7a72c36dbe1eea
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-12 13:41:54 +0200

    Moved data to initializer

--- init.rb
+++ init.rb
@@ -2,7 +2,5 @@ require 'starling'
 require 'simplified_starling'
 require 'simplified_starling/active_record'
 
-config = YAML.load_file("#{RAILS_ROOT}/config/starling/#{RAILS_ENV}.yml")
-host, port = config['starling']['host'], config['starling']['port']
 
-STARLING = Starling.new("#{host}:#{port}")
+# puts MY_STARLING.class
