cba34a585a68853144ba92c4cf2832865271226c
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-12 10:25:02 +0200

    Require ar

--- init.rb
+++ init.rb
@@ -1,8 +1,6 @@
 require 'starling'
 require 'simplified_starling'
-require 'model_extensions'
-
-ActiveRecord::Base.send(:include, ModelExtensions)
+require 'simplified_starling/active_record'
 
 config = YAML.load_file("#{RAILS_ROOT}/config/starling/#{RAILS_ENV}.yml")
 host, port = config['starling']['host'], config['starling']['port']
