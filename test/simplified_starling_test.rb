fc2499cda0096261aba3ddf968f44576c74c4ec9
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-26 11:56:47 +0200

    Test to confirm array class is not affected by method overwrite

--- test/simplified_starling_test.rb
+++ test/simplified_starling_test.rb
@@ -2,8 +2,10 @@ require 'test/unit'
 
 class SimplifiedStarlingTest < Test::Unit::TestCase
 
-  def test_this_plugin
-    assert true
+  def test_array_class_is_not_affected_by_method_overwrite
+    a = [ "a", "b", "c" ]
+    a.push("d", "e", "f")
+    assert_equal a, ["a", "b", "c", "d", "e", "f"]
   end
 
 end
