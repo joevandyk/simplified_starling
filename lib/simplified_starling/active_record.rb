6de966f51d3f48e30005a34ee109f0df12fea16d
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-26 18:43:33 +0200

    Added a mixin and updated push method with a logger

--- lib/simplified_starling/active_record.rb
+++ lib/simplified_starling/active_record.rb
@@ -1,29 +1,28 @@
 module SimplifiedStarling
 
+  ##
+  # Push record task into the queue
+  #
+  def push(task)
+    job = { :type => self.class.to_s, :id => (self.kind_of? Class) ? nil : self.id, :task => task }
+    STARLING.set(STARLING_CONFIG['starling']['queue'], job)
+    logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_starling.log")
+    model = job[:id] ? self.class : self.to_s
+    logger.info "Job: #{task.titleize.capitalize} #{model.to_s.downcase} #{job[:id]}"
+  end
+
+end
+
+module SimplifiedStarling
+
   class ActiveRecord::Base
-
-    ##
-    # Push record task into the queue
-    #
-    def push(task)
-      job = { :type => self.class.to_s, :id => self.id, :task => task }
-      STARLING.set(STARLING_CONFIG['starling']['queue'], job)
-    end
-
+    include SimplifiedStarling
   end
 
 end
 
 class Class
-
-  ##
-  # Push a class method task into the queue
-  #
-  def push(task)
-    job = { :type => self.to_s, :task => task }
-    STARLING.set(STARLING_CONFIG['starling']['queue'], job)
-  end
-
+  include SimplifiedStarling
 end
 
 ActiveRecord::Base.send(:include, SimplifiedStarling)
