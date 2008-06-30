a25591ec683c6d7c273377c33484c275db12ee0e
Francesc Esplugas <fesplugas@intraducibles.net>
2008-06-30 08:42:25 +0200

    Verify active_connections ...

--- lib/simplified_starling/active_record.rb
+++ lib/simplified_starling/active_record.rb
@@ -5,6 +5,8 @@ module SimplifiedStarling
   #
   def push(task)
 
+    ActiveRecord::Base.verify_active_connections! if defined? (ActiveRecord)
+
     job = {}
     job[:type] = (self.kind_of? Class) ? self.to_s : self.class.to_s
     job[:id] = (self.kind_of? Class) ? nil : self.id
