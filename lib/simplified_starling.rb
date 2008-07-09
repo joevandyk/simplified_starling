b4f5559492b67cbb07fa263fe44354a64ab5fed7
Francesc Esplugas <fesplugas@intraducibles.net>
2008-07-10 00:54:17 +0200

    Moved code to another method ...

--- lib/simplified_starling.rb
+++ lib/simplified_starling.rb
@@ -8,28 +8,32 @@ module Simplified
     end
 
     def self.start_processing(queue, daemon = true)
-      logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_starling.log")
       daemonize() if daemon
       loop do
-        job = STARLING.get(queue)
-        begin
-          if job[:id]
-            job[:type].constantize.find(job[:id]).send(job[:task])
-          else
-            job[:type].constantize.send(job[:task])
-          end
-          logger.info "[#{Time.now.to_s(:db)}] Popped #{job[:task]} on #{job[:type]} #{job[:id]}"
-        rescue ActiveRecord::RecordNotFound
-          logger.warn "[WARNING] #{job[:type]}##{job[:id]} gone from database."
-        rescue ActiveRecord::StatementInvalid
-          logger.warn "[WARNING] Database connection gone, reconnecting & retrying."
-          logger.info "          #{job.inspect}"
-          STARLING.set(STARLING_CONFIG['starling']['queue'], job)
-          logger.info "[#{Time.now.to_s(:db)}] Pushed #{job[:task]} on #{job[:type]} #{job[:id]}"
-          ActiveRecord::Base.connection.reconnect! and retry
-        rescue Exception => error
-          logger.error error
+        process(queue)
+      end
+    end
+
+    def self.process(queue)
+      logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_starling.log")
+      job = STARLING.get(queue)
+      begin
+        if job[:id]
+          job[:type].constantize.find(job[:id]).send(job[:task])
+        else
+          job[:type].constantize.send(job[:task])
         end
+        logger.info "[#{Time.now.to_s(:db)}] Popped #{job[:task]} on #{job[:type]} #{job[:id]}"
+      rescue ActiveRecord::RecordNotFound
+        logger.warn "[WARNING] #{job[:type]}##{job[:id]} gone from database."
+      rescue ActiveRecord::StatementInvalid
+        logger.warn "[WARNING] Database connection gone, reconnecting & retrying."
+        logger.info "          #{job.inspect}"
+        STARLING.set(STARLING_CONFIG['starling']['queue'], job)
+        logger.info "[#{Time.now.to_s(:db)}] Pushed #{job[:task]} on #{job[:type]} #{job[:id]}"
+        ActiveRecord::Base.connection.reconnect! and retry
+      rescue Exception => error
+        logger.error error
       end
     end
 
