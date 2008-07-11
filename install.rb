9a18f8a649299e0981220a8927d57b9969e21253
Francesc Esplugas <fesplugas@intraducibles.net>
2008-07-11 16:46:27 +0200

    Refactored to method

--- install.rb
+++ install.rb
@@ -1,16 +1,4 @@
 require 'fileutils'
+require File.dirname(__FILE__) + '/lib/simplified_starling'
 
-starling_folder = File.dirname(__FILE__) + '/../../../config/starling'
-
-FileUtils.mkdir starling_folder unless File.exist?(starling_folder)
-
-%w( development test production ).each do |env|
-  starling_config = File.dirname(__FILE__) + "/../../../config/starling/#{env}.yml"
-  FileUtils.cp File.dirname(__FILE__) + '/files/config.yml.tpl', starling_config unless File.exist?(starling_config)
-end
-
-starling_initializer = File.dirname(__FILE__) + '/../../../config/initializers/starling.rb'
-
-unless File.exist?(starling_initializer)
-  FileUtils.cp File.dirname(__FILE__) + '/files/initializer.rb', starling_initializer
-end
+Simplified::Starling.setup
