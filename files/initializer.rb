4506da202620b25855fcb3b4937f701dac069462
Francesc Esplugas <fesplugas@intraducibles.net>
2008-07-12 18:12:55 +0200

    Cleaned initializer and added comments

--- files/initializer.rb
+++ files/initializer.rb
@@ -1,4 +1,7 @@
-STARLING_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/starling/#{RAILS_ENV}.yml")
-host, port = STARLING_CONFIG['starling']['host'], STARLING_CONFIG['starling']['port']
 
-STARLING = Starling.new("#{host}:#{port}")
+##
+# Load starling setting and connect application to starling.
+#
+
+STARLING_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/starling/#{RAILS_ENV}.yml")['starling']
+STARLING = Starling.new("#{STARLING_CONFIG['host']}:#{STARLING_CONFIG['port']}")
