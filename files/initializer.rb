d10a665891a582b936132096cdcb80e1571e14ca
Francesc Esplugas <fesplugas@intraducibles.net>
2008-07-12 18:51:55 +0200

    Removed initializer

--- files/initializer.rb
+++
@@ -1,7 +0,0 @@
-
-##
-# Load starling setting and connect application to starling.
-#
-
-STARLING_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/starling/#{RAILS_ENV}.yml")['starling']
-STARLING = Starling.new("#{STARLING_CONFIG['host']}:#{STARLING_CONFIG['port']}")
